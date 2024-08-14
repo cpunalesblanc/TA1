9059b9e411dee5c0a687e4bc44ad1b863889c5b3
Jacqueline-03-mvd <jacquelinebarolin03@gmail.com>
2024-08-14 19:40:12 -0300

    commit push

--- js/validacion.js
+++ js/validacion.js
@@ -1,3 +1,15 @@
+document.getElementById("regBtn").addEventListener("click", function() {
+    const terminos = document.getElementById("terminos").checked;
+
+    if (!terminos) {
+        showAlertError();
+        return;
+    }
+
+    showAlertSuccess();
+});
+
+
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
