6e4f3f7405a470bcc7f9629549bfcdb27e3683e9
Cata Puñales <cpunalesblanc@gmail.com>
2024-08-14 21:16:25 -0300

    Cambios finals

--- js/validacion.js
+++ js/validacion.js
@@ -1,24 +1,4 @@
 document.getElementById("regBtn").addEventListener("click", function() {
-    const terminos = document.getElementById("terminos").checked;
-
-    if (!terminos) {
-        showAlertError();
-        return;
-    }
-
-    showAlertSuccess();
-});
-
-
-function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
-}
-
-function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
-}
-
-document.getElementById("regBtn").addEventListener("click", function() {
     validarFormulario();
 });
 
@@ -29,10 +9,33 @@ function validarFormulario() {
     const password1 = document.getElementById("password1").value;
     const password2 = document.getElementById("password2").value;
     const terminos = document.getElementById("terminos").checked;
-    
-    if (nombre === "" || apellido === "" || email === "" || password1 === "" || password2 === "" || !terminos) {
-        showAlertError();}
-        else {
-    showAlertSuccess();
-        }
+
+    if (nombre === "" || apellido === "" || email === "" || password1 === "" || password2 === "") {
+        showAlertError();
+        return;
     }
+
+    if (password1.length < 6) {
+        showAlertError();
+        return;
+    }
+
+    if (password1 !== password2) {
+        showAlertError();
+        return;
+    }
+
+    if (!terminos) {
+        showAlertError();
+        return;
+    }
+    showAlertSuccess();
+}
+
+function showAlertSuccess() {
+    document.getElementById("alert-success").classList.add("show");
+}
+
+function showAlertError() {
+    document.getElementById("alert-danger").classList.add("show");
+}
